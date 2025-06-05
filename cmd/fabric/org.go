8d15621411a9e5edb8532979b168c27d0afaa027
= <Arya@val.id>
2025-06-05 08:30:53 +0700

    feat: fabric config

---
+++ cmd/fabric/org.go
@@ -0,0 +1,17 @@
+package fabric
+
+import (
+	"github.com/hyperledger/fabric-gateway/pkg/client"
+)
+
+type OrgSetup struct {
+	OrgName      string
+	MSPID        string
+	CryptoPath   string
+	CertPath     string
+	KeyPath      string
+	TLSCertPath  string
+	PeerEndpoint string
+	GatewayPeer  string
+	Gateway      client.Gateway
+}
